73d958d7d6139e014dbb86260898e5c25af82d0b
hoainam201 <mayurijudgement24@gmail.com>
2022-12-21 19:41:30 +0700

    commit

---
+++ backend/src/database/data/datapg.sql
@@ -0,0 +1,13 @@
+insert into categories(categoryname, description)
+values ('Điện thoại', 'https://images.fpt.shop/unsafe/fit-in/60x60/filters:quality(90):fill(transparent)/fptshop.com.vn/Uploads/images/2022/iconcate/icon-mobile.png'),
+       ('Laptop', 'https://images.fpt.shop/unsafe/fit-in/60x60/filters:quality(90):fill(transparent)/fptshop.com.vn/Uploads/images/2022/iconcate/icon-laptop.png'),
+       ('Linh kiện PC', 'https://images.fpt.shop/unsafe/fit-in/60x60/filters:quality(90):fill(transparent)/fptshop.com.vn/Uploads/images/2022/iconcate/icon-pc.png'),
+       ('Máy tỉnh bảng', 'https://images.fpt.shop/unsafe/fit-in/60x60/filters:quality(90):fill(transparent)/fptshop.com.vn/Uploads/images/2022/iconcate/icon-tablet.png'),
+       ('Thiết bị thông minh', 'https://images.fpt.shop/unsafe/fit-in/60x60/filters:quality(90):fill(transparent)/fptshop.com.vn/Uploads/images/2022/iconcate/icon-smart.png'),
+       ('Gia dụng', 'https://images.fpt.shop/unsafe/fit-in/60x60/filters:quality(90):fill(transparent)/fptshop.com.vn/Uploads/images/2022/iconcate/icon-houseware.png'),
+       ('Apple', 'https://images.fpt.shop/unsafe/fit-in/60x60/filters:quality(90):fill(transparent)/fptshop.com.vn/Uploads/images/2022/iconcate/icon-apple.png'),
+       ('Samsung', 'https://images.fpt.shop/unsafe/fit-in/60x60/filters:quality(90):fill(transparent)/fptshop.com.vn/Uploads/images/2015/Other/icon-samsung.png'),
+       ('Đồng hồ thông minh', 'https://images.fpt.shop/unsafe/fit-in/60x60/filters:quality(90):fill(transparent)/fptshop.com.vn/Uploads/images/2022/iconcate/icon-smartwatch.png'),
+       ('Phụ kiện', 'https://images.fpt.shop/unsafe/fit-in/60x60/filters:quality(90):fill(transparent)/fptshop.com.vn/Uploads/images/2022/iconcate/icon-accessories.png'),
+       ('Màn hình', 'https://images.fpt.shop/unsafe/fit-in/60x60/filters:quality(90):fill(transparent)/fptshop.com.vn/Uploads/images/2022/iconcate/icon-screen.png'),
+       ('Máy cũ', 'https://images.fpt.shop/unsafe/fit-in/60x60/filters:qualitcy(90):fill(transparent)/fptshop.com.vn/Uploads/images/2022/iconcate/icon-tcdm.png');
---
+++ backend/src/database/data/demopg.sql
@@ -0,0 +1,121 @@
+create table categories
+(
+    categoryID     serial primary key,
+    categoryName   varchar not null,
+    categoryParent int,
+    description    text,
+    foreign key (categoryParent) references categories (categoryID)
+);
+
+create table suppliers
+(
+    supplierID  serial primary key,
+    companyName varchar not null,
+    country     varchar not null
+);
+
+create table products
+(
+    productID    serial primary key,
+    productName  varchar not null,
+    supplierID   int     not null,
+    categoryID   int     not null,
+    description  text,
+    price        int     not null,
+    point        int     not null,
+    unitOnOrders smallint,
+    discontinue  boolean default false,
+    foreign key (supplierID) references suppliers (supplierID),
+    foreign key (categoryID) references categories (categoryID)
+);
+
+create table productImg
+(
+    imgID     serial primary key,
+    img       bytea not null,
+    productID int,
+    foreign key (productID) references products (productID)
+);
+
+create table discounts
+(
+    discountID serial primary key,
+    startDate  date,
+    endDate    date
+);
+
+create table customers
+(
+    customerID       serial primary key,
+    phone            varchar unique,
+    passwords        varchar not null,
+    contactName      varchar not null,
+    contactTitle     varchar,
+    accumulatePoints int default 0,
+    address          varchar,
+    postalCode       varchar,
+    email            varchar
+);
+
+create table orders
+(
+    orderID      serial primary key,
+    customerID   int,
+    orderDate    date default current_date,
+    requiredDate date,
+    shippedDate  date,
+    shipAddress  varchar,
+    foreign key (customerID) references customers (customerID)
+);
+
+create table orderDetails
+(
+    orderID    serial,
+    productID  serial,
+    primary key (orderID, productID),
+    price      int not null,
+    quantity   int default 1,
+    discountID int,
+    foreign key (orderID) references orders (orderID),
+    foreign key (discountID) references discounts (discountID),
+    foreign key (productID) references products (productID)
+);
+
+create table admins
+(
+    adminID   serial primary key,
+    passwords varchar not null,
+    firstname varchar not null,
+    lastname  varchar not null,
+    phone     varchar unique,
+    email     varchar,
+    address   varchar
+);
+
+create table questions
+(
+    questionID  serial primary key,
+    productID   int references products (productID),
+    customerID  int references customers (customerID),
+    question    text not null,
+    createdTime timestamp
+);
+
+create table answers
+(
+    answerID    serial primary key,
+    questionID  int references questions (questionID),
+    adminID     int references admins (adminID),
+    answer      text not null,
+    createdTime timestamp
+);
+
+create table reviews
+(
+    reviewID    serial primary key,
+    productID   int references products (productID),
+    customerID  int references customers (customerID),
+    stars       int not null,
+    node        text,
+    createdTime timestamp
+)
