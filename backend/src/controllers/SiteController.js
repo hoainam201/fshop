ee99895bc4c645e73f8ffae4543b39babf0edaa0
hoainam201 <mayurijudgement24@gmail.com>
2023-01-04 18:46:29 +0700

    commit

--- backend/src/controllers/SiteController.js
+++ backend/src/controllers/SiteController.js
@@ -11,6 +11,15 @@ class SiteController{
         })
     }
 
+    home(req, res, next) {
+        const text = parseInt(req.params.text);
+        db.query('select * from products where productname = $1', [text], (err, result) => {
+            if (err) {
+                return next(err);
+            }
+            res.json(result.rows);
+        })
+    }
 }
 
 module.exports = new SiteController;
