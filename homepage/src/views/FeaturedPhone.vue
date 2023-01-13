0aed8eea21805ffc060bcd59f13ec27c50629106
haiduongfc2001 <haiduongfc2001@gmail.com>
2023-01-13 20:11:28 +0700

    ItemInfoPromo

--- homepage/src/views/FeaturedPhone.vue
+++ homepage/src/views/FeaturedPhone.vue
@@ -51,33 +51,7 @@
                                     {{ phoneproduct.memory}}
                                   </span>
                     </div>
-                    <div class="card-item-info__promo-detail">
-                                  <span>
-                                    <div class="lazyload-wrapper">
-                                      <img src="https://images.fpt.shop/unsafe/fit-in/45x45/filters:quality(90):fill(white)/fptshop.com.vn/Uploads/Originals/2021/7/1/637607496499646234_637528843647072230_balo%20(1).jpg"
-                                           alt="Tặng Balo Laptop" width="45" height="45">
-                                    </div>
-                                  </span>
-                      <span>
-                                    <div class="lazyload-wrapper">
-                                      <img src="https://images.fpt.shop/unsafe/fit-in/45x45/filters:quality(90):fill(white)/fptshop.com.vn/Uploads/Originals/2022/10/1/638002156152811193_Logo-VNPAYQR.png"
-                                           alt="Giảm 3% tối đa 250.000 qua Grab Moca" width="45" height="45">
-                                    </div>
-                                  </span>
-                      <span>
-                                    <div class="lazyload-wrapper">
-                                      <img src="https://images.fpt.shop/unsafe/fit-in/45x45/filters:quality(90):fill(white)/fptshop.com.vn/Uploads/Originals/2022/10/1/638002152487434133_637426739557050119_logo-moca.png"
-                                           alt="Tặng Balo Laptop" width="45" height="45">
-                                    </div>
-                                  </span>
-                      <span>
-                                    <div class="lazyload-wrapper">
-                                      <img src="https://images.fpt.shop/unsafe/fit-in/45x45/filters:quality(90):fill(white)/fptshop.com.vn/Uploads/Originals/2022/4/5/637847745475943329_637846629514363952_ic-tp-bank.jpg"
-                                           alt="Giảm ngay 600.000 khi mở thẻ TPBANK EVO" width="45" height="45">
-                                    </div>
-                                  </span>
-                      <div class="card-item-info__promo-text">Giảm ngay 600.000 khi mở thẻ TPBANK EVO</div>
-                    </div>
+                    <ItemInfoPromo />
                   </div>
                 </div>
               </div>
