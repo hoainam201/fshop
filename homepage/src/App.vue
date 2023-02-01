c8b0bed188f98baf91d5fe4c03a1de61c936c309
hwngngueyn <119343865+hwngngueyn@users.noreply.github.com>
2023-02-01 10:54:46 +0700

    Update App.vue

--- homepage/src/App.vue
+++ homepage/src/App.vue
@@ -1,15 +1,48 @@
 <template>
   <div class="app">
-    <Header />
-    <router-view/>
-    <Footer />
+    <div class="view">
+      <div :class="{'login-view': isSignUp, 'login-view-none': !isSignUp}"><Login :closeSignUp="closeSignUp"/></div>
+      <Header :openSignUp="openSignUp"/>
+      <router-view/>
+      <Footer />
+    </div>
   </div>
 </template>
 
-<style>
-
-</style>
 <script setup>
 import Header from "@/components/layouts/Header.vue";
 import Footer from "@/components/layouts/Footer.vue";
+import Login from "@/components/layouts/LoginForm/Login.vue"
 </script>
+
+<script>
+export default {
+  data() {
+    return {
+      isSignUp: false
+    }
+  },
+  methods: {
+    openSignUp() {
+      this.isSignUp = true
+      console.log('hello')
+    },
+    closeSignUp() {
+      this.isSignUp = false
+    }
+  }
+
+}
+</script>
+<style>
+.view {
+  position: relative;
+}
+.login-view {
+  position: absolute;
+  z-index: 1;
+}
+.login-view-none {
+  display: none;
+}
+</style>
