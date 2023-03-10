0c3c06986927d7cf08d214ef1c3ea89fec808fbb
haiduongfc2001 <haiduongfc2001@gmail.com>
2023-03-11 00:15:15 +0700

    test payment

--- Client/src/views/Test.vue
+++ Client/src/views/Test.vue
@@ -1,94 +1,61 @@
-<!--<template>-->
-<!--  <div>-->
-<!--      <h3 class="modal-product__name">-->
-<!--        {{ product.product.productname }} - {{ product.quantity }}-->
-<!--      </h3>-->
-<!--      <div class="cart__form cart__form&#45;&#45;type">-->
-<!--        <div class="cart__form__block">-->
-<!--          <div class="namecus">-->
-<!--            <input type="text" class="form-control" placeholder="Nhập họ và tên"-->
-<!--                   aria-label="Username" aria-describedby="addon-wrapping" required>-->
-<!--          </div>-->
-<!--          <div class="phonecus">-->
-<!--            <input type="tel" class="form-control" placeholder="Nhập số điện thoại"-->
-<!--                   aria-label="PhoneNumber" aria-describedby="addon-wrapping"-->
-<!--                   id="phoneNumber" v-model="phoneNumber" pattern="\d*"-->
-<!--                   :minlength="10" :maxlength="10" required>-->
-<!--          </div>-->
-<!--          <div>-->
-<!--            <div class="cart__title">Chọn hình thức thanh toán</div>-->
-<!--            <div class="cart__payment">-->
-<!--              <div class="form-check margin-right" style="width: 180px">-->
-<!--                <input readonly class="form-check-input" type="radio" name="paymentMethod"-->
-<!--                       id="cash" value="cash"-->
-<!--                       v-model="paymentMethod">-->
-<!--                <label class="form-check-label" for="cash">-->
-<!--                  Thanh toán tiền mặt-->
-<!--                </label>-->
-<!--              </div>-->
-<!--              <div class="form-check">-->
-<!--                <input class="form-check-input" type="radio"-->
-<!--                       id="online" value="online"-->
-<!--                       v-model="paymentMethod">-->
-<!--                <label class="form-check-label" for="online">-->
-<!--                  Thanh toán qua ví VNPay-->
-<!--                </label>-->
-<!--              </div>-->
-<!--            </div>-->
-<!--          </div>-->
-<!--        </div>-->
-<!--      </div>-->
-<!--      <div class="modal-footer">-->
-<!--        <button type="button" class="btn btn-xl cart-submit">HOÀN TẤT ĐẶT HÀNG</button>-->
-<!--      </div>-->
-<!--  </div>-->
-<!--</template>-->
-
-<!--<script>-->
-<!--export default {-->
-<!--  data() {-->
-<!--    return {-->
-<!--      products: [],-->
-<!--      product: {},-->
-<!--      itemCount: 0,-->
-<!--      order: [],-->
-<!--      cart: [],-->
-<!--      change: 0,-->
-<!--      quantity: 1,-->
-<!--      phoneNumber: '',-->
-<!--    }-->
-<!--  },-->
-<!--  created() {-->
-<!--    this.itemCount = JSON.parse(localStorage.getItem("order")).length;-->
-<!--    this.order = JSON.parse(localStorage.getItem("order"));-->
-<!--  },-->
-<!--}-->
-<!--</script>-->
-
 <template>
   <div>
     <div>
-      <span >{{ orderid }}</span>
-      <span >{{ contactname }}</span>
-      <span >{{ contactphone }}</span>
-    </div>
-    <div v-for="product in order" :key="product.product.productid" class="c-cart__product">
-      {{ order.productname }} - {{ order.quantity }} - {{  }}
+      <div></div>
+      <div class="modal-product__total">
+        <p class="text-normal">
+          <span>Tổng tiền:</span>
+          <span>{{ formatCurrency(totalPay) }}</span>
+        </p>
+        <p class="text-normal" style="margin-top: 4px">
+          <span>Giảm:</span>
+          <span>- {{ formatCurrency(discountPay) }}</span>
+        </p>
+        <p class="text--lg" style="margin-top: 8px">
+          <span class="text-size--lg">Cần thanh toán:</span>
+          <span class="re-price re-red priceFinal">{{ formatCurrency(needToPayment) }}</span>
+        </p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import {formatCurrency} from "@/utils";
+import axios from "axios";
 
 export default {
   name: 'Orders',
   data() {
     return {
-
+      order: {},
     }
+  },
+
+  computed: {
+    totalPay() {
+      return this.order.order ? this.order.order.reduce((total, product) => total + product.price * product.quantity, 0) : 0;
+    },
+    discountPay() {
+      return this.order.order ? this.order.order.reduce((total, product) => total + product.discount * product.quantity, 0) : 0;
+    },
+    needToPayment() {
+      return this.totalPay - this.discountPay;
+    },
+  },
+
+  methods: {
+    formatCurrency,
+  },
+
+  mounted() {
+    axios.get(`http://localhost:4000/order/${this.$route.params.id}`)
+        .then(response => {
+          this.order = response.data;
+        })
+        .catch(error => {
+          console.log(error);
+        });
   },
 }
 </script>
-
-
-
