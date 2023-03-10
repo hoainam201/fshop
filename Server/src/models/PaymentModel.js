0b72f95f85f3996c82124afc193462fd08a6089a
hoainam201 <mayurijudgement24@gmail.com>
2023-03-10 19:24:56 +0700

    pãyyyyy

--- Server/src/models/PaymentModel.js
+++ Server/src/models/PaymentModel.js
@@ -12,7 +12,7 @@ const addProduct = async (orderid, list) => {
     for (const key in list) {
         const product = list[key].product;
         console.log(product);
-        await db.query('insert into orderdetails values ($1, $2, $3, $4)', [orderid, product.productid, product.price - product.discount, list[key].quantity]);
+        await db.query('insert into orderdetails values ($1, $2, $3, $4, $5)', [orderid, product.productid, product.price, product.discount, list[key].quantity]);
         total += (product.price - product.discount) * list[key].quantity;
     }
     return total;
@@ -30,13 +30,19 @@ const returnUrl = async (id) => {
 
 const getOrder = async (id) => {
     let {rows} = await db.query('select * from orders where orderid = $1', [id]);
-    const list = await db.query('select (select productname from products where orderdetails.productid = products.productid), price, quantity, (select img from productimg where orderdetails.productid = productimg.productid limit 1) from orderdetails where orderid = $1', [id]);
+    const list = await db.query('select (select productname from products where orderdetails.productid = products.productid), price, discount, quantity, (select img from productimg where orderdetails.productid = productimg.productid limit 1) from orderdetails where orderid = $1', [id]);
     let total = 0;
+    let discount = 0;
     for (const k in list.rows){
         const detail = list.rows[k];
-        total += detail.price * detail.quality;
+        console.log(detail.price + ">" + detail.discount + ">" + detail.quantity);
+        total += detail.price * detail.quantity;
+        discount += detail.discount * detail.quantity;
     }
     rows[0].order = list.rows;
+    rows[0].total = total;
+    rows[0].discount = discount;
+    rows[0].lastTotal = total - discount;
     console.log(rows);
     return rows[0];
 }
